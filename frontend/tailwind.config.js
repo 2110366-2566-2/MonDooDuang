982e30c17ca1c711b082c043605f33ae4f2b80b0
IamMrTeapot <87607928+IamMrTeapot@users.noreply.github.com>
2024-01-29 23:21:25 +0700

    fix: tailwind extends default palette

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,13 +2,14 @@
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    colors: {
-      "example-color": "#ff0000"
-    },
-    fontFamily: {
-      "example-font": ["Inter var", "sans-serif"]
-    },
-    extend: {}
+    extend: {
+      colors: {
+        "example-color": "#ff0000"
+      },
+      fontFamily: {
+        "example-font": ["Inter var", "sans-serif"]
+      }
+    }
   },
   plugins: []
 }
