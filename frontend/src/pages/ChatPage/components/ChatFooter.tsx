082a068619273c8a514a42f2bd5a1ad0556da2aa
Sirawit Pulketkij <sirawit.pulketkij@gmail.com>
2024-02-04 02:42:17 +0700

    install mui + add first version of chatt footer

--- frontend/src/pages/ChatPage/components/ChatFooter.tsx
+++ frontend/src/pages/ChatPage/components/ChatFooter.tsx
@@ -1,3 +1,4 @@
+import EventAvailableIcon from "@mui/icons-material/EventAvailable"
 interface ChatFooterProps {
   messageText: string
   setMessageText: (message: string) => void
@@ -6,16 +7,18 @@ interface ChatFooterProps {
 
 export default function ChatFooter({ messageText, setMessageText, sendMessage }: ChatFooterProps) {
   return (
-    <>
-      <div className="input-box">
-        <input
-          type="text"
-          value={messageText}
-          onChange={(e) => setMessageText(e.target.value)}
-          placeholder="Type your message..."
-        />
-        <button onClick={sendMessage}>Send</button>
-      </div>
-    </>
+    <div className="w-[821px] h-[59px] bg-gray-300 p-4 flex justify-center items-center">
+      <EventAvailableIcon />
+      <input
+        type="text"
+        value={messageText}
+        onChange={(e) => setMessageText(e.target.value)}
+        placeholder="Type your message..."
+        className="flex-grow p-2 border border-gray-400 rounded-md mr-2"
+      />
+      <button onClick={sendMessage} className="bg-blue-500 text-white px-4 py-2 rounded-md">
+        Send
+      </button>
+    </div>
   )
 }
--- frontend/src/pages/ChatPage/components/ChatSidebar.tsx
+++ frontend/src/pages/ChatPage/components/ChatSidebar.tsx
@@ -1,4 +1,5 @@
 import ChatList from "./ChatList"
+import SearchIcon from "@mui/icons-material/Search"
 interface ChatSidebarProps {
   conversationIds: string[]
 }
@@ -6,11 +7,14 @@ interface ChatSidebarProps {
 export default function ChatSidebar({ conversationIds }: ChatSidebarProps) {
   return (
     <div className="overflow-y-auto h-screen flex flex-col items-center justify-start">
-      <input
-        type="text"
-        placeholder="ค้นหาข้อความและห้องแชท"
-        className="h-[32px] w-[307px] mt-[10px] p-2 rounded-3xl bg-gray-300 text-white placeholder-white"
-      />
+      <div className="relative">
+        <SearchIcon />
+        <input
+          type="text"
+          placeholder={"ค้นหาข้อความและห้องแชท"}
+          className="h-[32px] w-[307px] mt-[10px] p-2 rounded-3xl bg-gray-300 text-white placeholder-white"
+        />
+      </div>
       {conversationIds.map((conversationId) => (
         <ChatList conversationId={conversationId} key={conversationId} />
       ))}
