7fe808d2e75dffa54faf5197a4a041b5e3ad3759
IamMrTeapot <87607928+IamMrTeapot@users.noreply.github.com>
2024-02-04 18:41:26 +0700

    fix: remove rules with parser in eslint

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -17,7 +17,6 @@ module.exports = {
     semi: ["error", "never"],
     "@typescript-eslint/explicit-function-return-type": "warn",
     "@typescript-eslint/no-unused-vars": "warn",
-    "@typescript-eslint/consistent-type-imports": "off",
-    "@typescript-eslint/no-misused-promises": "warn"
+    "@typescript-eslint/consistent-type-imports": "off"
   }
 }
