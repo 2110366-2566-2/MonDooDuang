7d5e5f9d3431bad3015e29b771f82c199e9cde82
Tittaya.W <97596208+Prince-in-the-wood@users.noreply.github.com>
2024-02-06 18:56:08 +0700

    fix: Change path to be more specific

--- backend/src/routes/report.routes.ts
+++ backend/src/routes/report.routes.ts
@@ -1,9 +1,9 @@
-import express from "express";
-import {reportController} from "../controllers/report/report.controller"
+import express from "express"
+import { reportController } from "../controllers/report/report.controller"
 
 const router = express.Router()
 
-router.post("/",reportController.createReport)
-router.get("/:conversationId/:userId",reportController.getReporteeId)
+router.post("/create-report", reportController.createReport)
+router.get("/reportee/:conversationId/:userId", reportController.getReporteeId)
 
 export default router
