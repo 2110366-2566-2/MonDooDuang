535c4ebf82f7d4774ea8df159569923fb281f7d7
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2024-02-09 21:01:19 +0700

    fix: wrong controller call

--- backend/src/routes/chat.routes.ts
+++ backend/src/routes/chat.routes.ts
@@ -6,5 +6,5 @@ const router = express.Router()
 router.get("/:userId", chatController.getConversationsByUserId)
 router.get("/lastMessage/:conversationId/:userId", chatController.getNameWithLastMessage)
 router.get("/messages/:conversationId/:userId", chatController.getMessagesByConversationId)
-router.get("/name/:conversationId/:userId", chatController.getMessagesByConversationId)
+router.get("/name/:conversationId/:userId", chatController.getNameByConversationId)
 export default router
