7a1ed8c536a5b690e43ceebcd7cc3e3847e71cb1
IamMrTeapot <87607928+IamMrTeapot@users.noreply.github.com>
2024-02-09 15:13:43 +0700

    feat: update logo

---
+++ frontend/src/common/components/NavBar/LogoIcon.tsx
@@ -0,0 +1,9 @@
+import logoIcon from "../../../assets/images/common/mdd_icon.png"
+
+export default function LogoIcon() {
+  return (
+    <div>
+      <img src={logoIcon} alt="logout icon" className="h-[50px]" />
+    </div>
+  )
+}
