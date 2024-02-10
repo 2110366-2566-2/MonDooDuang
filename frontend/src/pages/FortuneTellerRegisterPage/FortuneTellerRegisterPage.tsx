01ecf2d14b57343d80e250871431efc7a5a35a13
Namprik <wareesa1847@gmail.com>
2024-02-10 12:17:12 +0700

    edit logo background

--- frontend/src/pages/FortuneTellerRegisterPage/FortuneTellerRegisterPage.tsx
+++ frontend/src/pages/FortuneTellerRegisterPage/FortuneTellerRegisterPage.tsx
@@ -1,25 +1,23 @@
-import FrameRegister from "../../assets/FortuneTellerRegisterAssets/FrameRegister.svg"
-import Background from "../../assets/FortuneTellerRegisterAssets/Background.png"
 import FormRegister from "./components/Form/FormRegister"
+import Logo from "../../assets/FortuneTellerRegisterAssets/Logo.svg"
 
 export default function FortuneTellerRegisterPage() {
   return (
-    <div
-      className="text-white bg-black bg-cover bg-no-repeat"
-      style={{ backgroundImage: `url(${Background})` }}
-    >
-      <div
-        style={{ backgroundImage: `url(${FrameRegister})` }}
-        className=" bg-no-repeat size-11/12 mx-auto"
-      >
-        <div className="flex flex-col pt-36">
-          {/* <div> */}
-          <h1 className="font-semibold text-4xl text-center">ยืนยันตัวตน</h1>
-          <div className="ml-36 pb-24">
-            <FormRegister></FormRegister>
-          </div>
+    <div className="h-full w-full bg-black flex flex-col items-center">
+      <div className="flex w-[30%]">
+        <div className="z-10 w-[30%] bg-gradient-to-l from-black" />
+        <div className="z-10 w-[40%] justify-center flex bg-black">
+          <img className="mt-4" src={Logo} />
+        </div>
+        <div className="z-10 w-[30%] bg-gradient-to-r from-black" />
+      </div>
+      <div className="flex flex-col mt-11 mb-0 text-white size-11/12">
+        <h1 className="font-semibold text-4xl text-center">ยืนยันตัวตน</h1>
+        <div className="ml-36 pb-24">
+          <FormRegister></FormRegister>
         </div>
       </div>
+      {/* </div> */}
     </div>
   )
 }
