0260c1dac464738fa8b7b583b61a7e2d97d1a760
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2024-02-10 08:46:30 +0700

    fix: await promise

--- backend/src/configs/socketConnection.ts
+++ backend/src/configs/socketConnection.ts
@@ -13,9 +13,9 @@ export const connectToSocket = (app: Application) => {
     }
   })
 
-  io.on("connection", (socket) => {
+  io.on("connection", async (socket) => {
     console.log("User connected:", socket.id)
-    conversationService.sendMessage(socket)
+    await conversationService.sendMessage(socket)
     conversationService.joinRoom(socket)
     conversationService.disconnect(socket)
   })
