0fe484d7187c215beeb418ec817a13a5469b80ea
Kittitat Tuntisak <58716471+IamKittitat@users.noreply.github.com>
2024-02-11 21:03:21 +0700

    [MD-20] Conversation Feature (#11)
    Co-authored-by: Sirawit Pulketkij <sirawit.pulketkij@gmail.com>
    Co-authored-by: IamMrTeapot <87607928+IamMrTeapot@users.noreply.github.com>

---
+++ frontend/src/pages/ConversationPage/components/MessageList.tsx
@@ -0,0 +1,31 @@
+import Message from "./Message"
+import { useEffect, useRef } from "react"
+import { MessageInformation } from "../types/MessageInformation"
+
+export default function MessageList({ messages }: { messages: MessageInformation[] }) {
+  const messagesEndRef = useRef<HTMLDivElement>(null)
+
+  const scrollToBottom = () => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth" })
+    }
+  }
+
+  useEffect(() => {
+    scrollToBottom() // Scroll to bottom when component first renders
+  }, [messages])
+
+  return (
+    <div className="flex flex-col overflow-scroll h-screen">
+      {messages.map((message, index) => (
+        <Message
+          key={index}
+          message={message.message}
+          sender={message.sender}
+          timeSent={message.timeSent}
+        />
+      ))}
+      <div ref={messagesEndRef} />
+    </div>
+  )
+}
--- frontend/src/routes/getPagesData.tsx
+++ frontend/src/routes/getPagesData.tsx
@@ -6,7 +6,7 @@ import AccountApp from "../pages/AccountPage/AccountApp"
 import FortuneTellerAccountApp from "../pages/FortuneTellerAccountPage/FortuneTellerAccountApp"
 import FortuneTellerPackageApp from "../pages/FortuneTellerPackagePage/FortuneTellerPackageApp"
 import SearchApp from "../pages/SearchPage/SearchApp"
-import ChatApp from "../pages/ChatPage/ChatApp"
+import ConversationApp from "../pages/ConversationPage/ConversationApp"
 import AppointmentApp from "../pages/AppointmentPage/AppointmentApp"
 import AdminApprovalApp from "../pages/AdminApprovalPage/AdminApprovalApp"
 import FortuneTellerDetailApp from "../pages/FortuneTellerDetailPage/FortuneTellerDetailApp"
@@ -53,8 +53,8 @@ export const getPagesData = (stripePromise: any) =>
       element: <FortuneTellerDetailApp />
     },
     {
-      path: "/chat",
-      element: <ChatApp />
+      path: "/conversation",
+      element: <ConversationApp />
     },
     {
       path: "/appointment",
