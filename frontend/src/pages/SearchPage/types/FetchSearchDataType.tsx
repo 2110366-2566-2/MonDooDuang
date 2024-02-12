2bad8dbe3d8576b3b5fa94613ed173860a8d4013
its144prelationship <pastalonely@gmail.com>
2024-02-12 10:53:34 +0700

    refactor: add type

---
+++ frontend/src/pages/SearchPage/types/FetchSearchDataType.tsx
@@ -0,0 +1,14 @@
+type FetchSearchData = {
+    current_packageid: string
+    current_speciality: string
+    fname: string
+    fortunetellerid: string
+    maxprice: number
+    minprice: number
+    packageid_list: string
+    profilepicture: string | null
+    speciality_list: string
+    stagename: string | null
+    totalreview: number
+    totalscore: number
+  }
