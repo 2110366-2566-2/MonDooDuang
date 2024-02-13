325ac3491ccf6351e9f5c8709be703f800ebd6aa
IamMrTeapot <87607928+IamMrTeapot@users.noreply.github.com>
2024-02-13 19:58:48 +0700

    feat: styling of payment feature

--- frontend/src/pages/PaymentCompletedPage/PaymentCompletedApp.tsx
+++ frontend/src/pages/PaymentCompletedPage/PaymentCompletedApp.tsx
@@ -1,9 +1,12 @@
+import NavBar from "../../common/components/NavBar/NavBar"
+import RootLayout from "../../common/components/RootLayout/RootLayout"
 import PaymentCompletedPage from "./PaymentCompletedPage"
 
 export default function PaymentCompletedApp() {
   return (
-    <>
+    <RootLayout>
+      <NavBar isFortuneTeller={false} menuFocus={"search"} username={"Payment"} />
       <PaymentCompletedPage />
-    </>
+    </RootLayout>
   )
 }
--- frontend/src/pages/PaymentCompletedPage/PaymentCompletedPage.tsx
+++ frontend/src/pages/PaymentCompletedPage/PaymentCompletedPage.tsx
@@ -1,7 +1,7 @@
 export default function PaymentCompletedPage() {
   return (
-    <div className="w-screen h-screen flex justify-center items-center text-7xl">
-      <h1>Payment Successful</h1>
+    <div className="w-full py-16 flex flex-col items-center text-7xl">
+      <h1 className="text-white mt-[100px]">Payment Successful</h1>
     </div>
   )
 }
