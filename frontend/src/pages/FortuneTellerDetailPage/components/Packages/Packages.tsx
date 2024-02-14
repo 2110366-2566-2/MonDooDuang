a455bc7e995250a46b8e27c58e856fc145f21c93
ponnabook <88271077+ponnabook@users.noreply.github.com>
2024-02-14 15:05:40 +0700

    fix: :zap: move check null to html part

--- frontend/src/pages/FortuneTellerDetailPage/components/Packages/Packages.tsx
+++ frontend/src/pages/FortuneTellerDetailPage/components/Packages/Packages.tsx
@@ -21,17 +21,12 @@ export default function Packages() {
     fetchFortuneTellerPackage()
   }, [])
 
-  let packageItems = null
-  if(fortuneTellerPackage !== undefined){
-    packageItems = fortuneTellerPackage.map((packageItem) => <PackageList packageItem = {packageItem}></PackageList>)   
-  }
-
   return (
     <div className="border-white border-[1px] w-[50%] min-w-[500px] sm:rounded-lg flex justify-center h-fit">
       <table className="w-[98%] max-h-[600px] h-auto">
         <thead className="text-[40px] font-libre-bodoni text-center leading-loose border-b-[1px] border-white">Packages</thead>
         <tbody className="flex flex-col overflow-x-auto overflow-y-auto">
-          {packageItems}
+        { fortuneTellerPackage && fortuneTellerPackage.map((packageItem) => <PackageList packageItem = {packageItem}></PackageList>) }
         </tbody>
       </table>
     </div>
