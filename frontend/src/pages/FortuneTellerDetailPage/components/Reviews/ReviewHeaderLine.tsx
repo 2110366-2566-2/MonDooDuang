05a18606d535bb3f79a6ff2267e5d3e1cd8fef6b
ponnabook <88271077+ponnabook@users.noreply.github.com>
2024-02-14 15:09:29 +0700

    fix: :zap: Edit svg file

--- frontend/src/pages/FortuneTellerDetailPage/components/Reviews/ReviewHeaderLine.tsx
+++ frontend/src/pages/FortuneTellerDetailPage/components/Reviews/ReviewHeaderLine.tsx
@@ -4,8 +4,8 @@ export default function ReviewHeaderLine() {
       <path d="M0.669873 5L5 9.33013L9.33013 5L5 0.669873L0.669873 5ZM5 5.75H1103V4.25H5V5.75Z" fill="url(#paint0_linear_51_1916)"/>
       <defs>
         <linearGradient id="paint0_linear_51_1916" x1="908.5" y1="4.99739" x2="1061.5" y2="5.00034" gradientUnits="userSpaceOnUse">
-          <stop stop-color="white"/>
-          <stop offset="1" stop-color="white" stop-opacity="0"/>
+          <stop stopColor="white"/>
+          <stop offset="1" stopColor="white" stopOpacity="0"/>
         </linearGradient>
       </defs>
     </svg>
--- frontend/src/pages/FortuneTellerDetailPage/components/Reviews/UserIcon.tsx
+++ frontend/src/pages/FortuneTellerDetailPage/components/Reviews/UserIcon.tsx
@@ -5,8 +5,8 @@ export default function UserIcon() {
         <path d="M38 26C41.4478 26 44.7544 27.2818 47.1924 29.5635C49.6304 31.8452 51 34.9399 51 38.1667C51 41.3935 49.6304 44.4881 47.1924 46.7698C44.7544 49.0515 41.4478 50.3333 38 50.3333C34.5522 50.3333 31.2456 49.0515 28.8076 46.7698C26.3696 44.4881 25 41.3935 25 38.1667C25 34.9399 26.3696 31.8452 28.8076 29.5635C31.2456 27.2818 34.5522 26 38 26ZM38 56.4167C52.365 56.4167 64 61.8612 64 68.5833V74.6667H12V68.5833C12 61.8612 23.635 56.4167 38 56.4167Z" fill="#FFD233"/>
       </g>
       <defs>
-        <filter id="filter0_i_809_9174" x="0" y="0" width="77" height="77" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-          <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <filter id="filter0_i_809_9174" x="0" y="0" width="77" height="77" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+          <feFlood floodOpacity="0" result="BackgroundImageFix"/>
           <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
           <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
           <feOffset dx="4" dy="4"/>
