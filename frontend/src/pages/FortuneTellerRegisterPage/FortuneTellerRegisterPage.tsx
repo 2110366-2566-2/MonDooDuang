dd400df8ada146183ab548a8f1a722397393229e
Namprik <wareesa1847@gmail.com>
2024-02-23 10:24:04 +0700

    add authProvider in fortuneTellerRegister

--- frontend/src/pages/FortuneTellerRegisterPage/FortuneTellerRegisterPage.tsx
+++ frontend/src/pages/FortuneTellerRegisterPage/FortuneTellerRegisterPage.tsx
@@ -1,20 +1,26 @@
+import { useContext } from "react"
+import { AuthContext } from "../../common/providers/AuthProvider"
 import FormRegister from "./components/Form/FormRegister"
 import Logo from "../../assets/FortuneTellerRegisterAssets/Logo.svg"
-import RootLayout from "../../common/components/RootLayout/RootLayout"
+import { useNavigate } from "react-router-dom"
 
 export default function FortuneTellerRegisterPage() {
-  const mockUserId = "84885c07-43d7-42b8-8919-88263a33fc74"
+  const { userId, userType } = useContext(AuthContext)
+  const navigate = useNavigate()
+  if (userType !== "CUSTOMER") {
+    navigate("/search")
+    return
+  }
+
   return (
-    <RootLayout>
-      <div className="z-50 flex flex-col items-center relative text-white font-noto-sans">
-        <img className="z-50 flex flex-col items-center mt-7 absolute" src={Logo} />
-        <div className="bg-transparent bg-black -z-[78] rounded-3xl border my-20 w-11/12 ">
-          <h1 className="font-semibold text-4xl text-center mt-24">ยืนยันตัวตน</h1>
-          <div className="ml-36 pb-12">
-            <FormRegister userId={mockUserId} />
-          </div>
+    <div className="z-50 flex flex-col items-center relative text-white font-noto-sans">
+      <img className="z-50 flex flex-col items-center mt-7 absolute" src={Logo} />
+      <div className="bg-transparent bg-black -z-[78] rounded-3xl border my-20 w-11/12 ">
+        <h1 className="font-semibold text-4xl text-center mt-24">ยืนยันตัวตน</h1>
+        <div className="ml-36 pb-12">
+          <FormRegister userId={userId} />
         </div>
       </div>
-    </RootLayout>
+    </div>
   )
 }
