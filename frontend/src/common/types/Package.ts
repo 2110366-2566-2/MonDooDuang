d7fb453a1db691eef7a11d815446faf5acd9d5b4
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2024-02-24 14:37:36 +0700

    feat: add common types

---
+++ frontend/src/common/types/Package.ts
@@ -0,0 +1,9 @@
+export type SpecialityType = 'TAROT_CARD' | 'ORACLE' | 'THAI' | 'NUMBER' | 'RUNES'
+
+export const specialityMapper: Record<SpecialityType,string> = {
+  TAROT_CARD : "ไพ่ทาโร่",
+  THAI : "โหราศาสตร์ไทย",
+  NUMBER : "ศราตร์ตัวเลข",
+  ORACLE :"ไพ่ออราเคิล",
+  RUNES : "ศาสตร์รูนส์"
+}
