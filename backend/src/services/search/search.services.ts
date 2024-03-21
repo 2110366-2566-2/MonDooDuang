d40597dc9d84a984c7c0dc2ed81eef87f985c647
its144prelationship <pastalonely@gmail.com>
2024-03-21 11:48:01 +0700

    feat: integrate s3 function to search api

--- backend/src/services/search/search.services.ts
+++ backend/src/services/search/search.services.ts
@@ -1,9 +1,21 @@
 import { searchRepository } from "../../repositories/search.repository"
 import { SearchSchema } from "../../models/search/search.model"
+import { s3Service } from "../infra/s3.services"
 
 export const searchService = {
   searchFortuneteller: async (searchOption: SearchSchema) => {
     const searchData = await searchRepository.searchFortuneteller(searchOption)
-    return searchData
+    if (!searchData) return null
+    const updatedSearchData = await Promise.all(searchData.map(async (data) => {
+      const fortuneTellerId = data.fortune_teller_id?.toString() ?? ""
+      const profilePicData = await s3Service.downloadProfilePicture(fortuneTellerId as string)
+      if (profilePicData?.ContentType !== undefined && profilePicData.ContentType !== null) {
+        data.profile_picture = "data:image/jpg;base64," + profilePicData.Body?.toString("base64")
+      } else {
+        data.profile_picture = null
+      }
+      return data
+    }))
+    return updatedSearchData
   }
 }
