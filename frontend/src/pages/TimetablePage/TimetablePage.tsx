94b103633ea5aa01f3ac9b5b235ea76e9548fc36
its144prelationship <pastalonely@gmail.com>
2024-03-21 13:22:10 +0700

    feat: auth context

--- frontend/src/pages/TimetablePage/TimetablePage.tsx
+++ frontend/src/pages/TimetablePage/TimetablePage.tsx
@@ -1,4 +1,9 @@
+import { useContext } from "react"
+import { AuthContext } from "../../common/providers/AuthProvider"
+
 export default function TimetablePage(): JSX.Element {
+  const { userId, userType, username } = useContext(AuthContext)
+  console.log(userId, userType, username)
   return (
     <div>
     </div>
