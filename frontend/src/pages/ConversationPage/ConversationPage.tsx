6d2271df707b184430f1407624ce0fef22b50fad
Tittaya.W <97596208+Prince-in-the-wood@users.noreply.github.com>
2024-03-22 17:54:17 +0700

    [MD-140] (Bug) get user type from conversation (#66)

--- frontend/src/pages/ConversationPage/ConversationPage.tsx
+++ frontend/src/pages/ConversationPage/ConversationPage.tsx
@@ -4,15 +4,25 @@ import ConversationBox from "./components/ConversationBox"
 import NavBar from "../../common/components/NavBar/NavBar"
 import { useContext, useState } from "react"
 import { AuthContext } from "../../common/providers/AuthProvider"
+import { ConversationService } from "./services/ConversationService"
 
 export default function ConversationPage() {
   const [isShowReport, setIsShowReport] = useState(false)
   const [selectedConversationId, setSelectedConversationId] = useState<string | null>(null)
   const { userId, userType, username } = useContext(AuthContext)
   const [isSystemReport, setIsSystemReport] = useState(false)
+  const [selectedUserType, setSelectedUserType] = useState<"CUSTOMER" | "FORTUNE_TELLER">(
+    "FORTUNE_TELLER"
+  )
 
-  const handleConversationSelect = (conversationId: string) => {
+  const handleConversationSelect = async (conversationId: string) => {
     setSelectedConversationId(conversationId)
+
+    const userTypeInConversation = await ConversationService.getUserTypeInConversation(
+      conversationId,
+      userId
+    )
+    setSelectedUserType(userTypeInConversation)
   }
 
   const showReport = () => {
@@ -45,13 +55,13 @@ export default function ConversationPage() {
             showReport={showReport}
             systemReport={systemReport}
             userId={userId}
-            userType={userType}
+            userType={selectedUserType}
           />
         </div>
         <ReportModal
           isShowReport={isShowReport}
           setIsShowReport={setIsShowReport}
-          isCustomer={userType === "CUSTOMER"}
+          isCustomer={selectedUserType === "CUSTOMER"}
           userId={userId}
           conversationId={selectedConversationId}
           isSystemReport={isSystemReport}
