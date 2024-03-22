4a982b37ee425125912f7b7afb61eea8098d8b9c
Namprik <wareesa1847@gmail.com>
2024-03-22 18:16:47 +0700

    add verification notification

---
+++ frontend/src/common/components/NavBar/Notification/VerificationNotification.tsx
@@ -0,0 +1,39 @@
+import { NotificationTypes } from "../../../types/NotificationTypes"
+import { showFullDate, showTime } from "../../../utils/FormatUtils"
+
+export default function VerificationNotification({
+  notification
+}: {
+  notification: NotificationTypes
+}) {
+  return (
+    <div className="flex flex-col gap-2">
+      {notification.notificationType === "VERIFICATION" ? (
+        <>
+          <div className="flex gap-1">
+            <div>คุณได้รับการ</div>
+            <div className="font-semibold text-mdd-yellow600">รับรอง</div>
+            <div>จากผู้ดูแล</div>
+          </div>
+          <div>กรุณา log out เพื่อใช้งานในฐานะหมอดู</div>
+        </>
+      ) : (
+        <>
+          <div className="flex gap-1">
+            <div>คุณถูก</div>
+            <div className="font-semibold text-mdd-yellow600">ปฏิเสธ</div>
+            <div>จากผู้ดูแล</div>
+          </div>
+          <div>กรุณาติดต่ออีกครั้งเพื่อยืนยันตัวตน</div>
+        </>
+      )}
+      <div className="flex self-end gap-1 text-gray-500 text-sm">
+        <div>วันที่</div>
+        <div>{showFullDate(notification.updatedAt)}</div>
+        <div>เวลา</div>
+        <div>{showTime(notification.updatedAt, 0)}</div>
+        <div>น.</div>
+      </div>
+    </div>
+  )
+}
