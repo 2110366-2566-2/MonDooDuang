e4cb80659051fd0381dce359ada249ec159eb5ab
Tinnaung <6430129921@student.chula.ac.th>
2024-03-22 16:24:57 +0700

    feat: :sparkles: frontend handler for each button

--- frontend/src/pages/AdminReportManagementPage/AdminReportManagementPage.tsx
+++ frontend/src/pages/AdminReportManagementPage/AdminReportManagementPage.tsx
@@ -5,6 +5,15 @@ import { PageLine } from "./components/Icon"
 import ReportCard from "./components/ReportCard"
 import { ReportInfoType } from "./types/ReportInfoType"
 import { ReportManagementService } from "./services/ReportManagementService"
+import {
+  ConfirmOverlay,
+  ConfirmOverlayProps
+} from "../../common/components/ConfirmOverlay/ConfirmOverlay"
+import {
+  SuccessModal,
+  SuccessModalProps
+} from "../../common/components/SuccessOverlay/SuccessOverlay"
+import { ErrorModal } from "../../common/components/ErrorOverlay/ErrorOverlay"
 
 export default function AdminReportManagementPage() {
   const { username } = useContext(AuthContext)
@@ -16,12 +25,151 @@ export default function AdminReportManagementPage() {
     "รายงานพฤติกรรมที่ไม่เหมาะสม",
     "รายงานเกี่ยวกับระบบ"
   ]
-
   const [behaviorReports, setBehaviorReports] = useState<ReportInfoType[]>([])
   const [moneyReports, setMoneyReports] = useState<ReportInfoType[]>([])
   const [systemReports, setSystemReports] = useState<ReportInfoType[]>([])
   const [menuList, setMenuList] = useState(defualtMenuList)
   const [focusReports, setFocusReports] = useState<ReportInfoType[]>([])
+
+  //overlay
+  const defaultConfirmProps: ConfirmOverlayProps = {
+    onClose: () => {
+      setIsConfirmModalOpen(false)
+    },
+    onConfirm: () => {
+      setIsConfirmModalOpen(false)
+      if (menuList[1] === "รายงานหมอดูไม่มาตามนัดหมาย") {
+        setIsSuccessModalOpen(true)
+      }
+    },
+    target: "หมาแดงดูดวง",
+    type: "REJECT",
+    title: "ต้องการแบนผู้ใช้",
+    message: "ใช่หรือไม่",
+    info: `ตอนนี้ระบบได้ระงับการโอนเงินไปยังหมอดูเอาไว้
+  \nหากกดยืนยัน ระบบจะดำเนินการโอนเงินให้หมอดูตามปกติ`
+  }
+  const defaultSuccessProps: SuccessModalProps = {
+    onClose: () => {
+      setIsSuccessModalOpen(false)
+    },
+    title: "ส่งคำขอจองไปที่หมอดูสำเร็จ",
+    info: `ผู้ใช้งานนี้จะไม่สามารถใช้งาน\nMonDooDuang ได้อีกต่อไป`
+  }
+  const banSuccessProps: SuccessModalProps = {
+    onClose: () => {
+      setIsSuccessModalOpen(false)
+    },
+    title: "แบนผู้ใช้งานสำเร็จ",
+    info: `ผู้ใช้งานนี้จะไม่สามารถใช้งาน\nMonDooDuang ได้อีกต่อไป`
+  }
+  const setUnsuspendedConfirmProps = (report: ReportInfoType) => {
+    const unsuspenedConfirmProps: ConfirmOverlayProps = {
+      onClose: () => {
+        setIsConfirmModalOpen(false)
+      },
+      onConfirm: () => {
+        setIsConfirmModalOpen(false)
+        setUnsuspendedSuccessProps()
+        setIsSuccessModalOpen(true)
+      },
+      target: report.reporteeName,
+      type: "REJECT",
+      title: "ต้องการจ่ายเงินให้หมอดูตามปกติ",
+      message: "ใช่หรือไม่",
+      info: `ตอนนี้ระบบได้ระงับการโอนเงินไปยังหมอดูเอาไว้\nหากกดยืนยัน ระบบจะดำเนินการโอนเงินให้หมอดูตามปกติ`,
+      warning: `หากกดแบนไปแล้วผู้ใช้จะไม่สามารถใช้งาน MonDooDuang ได้อีก`
+    }
+    setConfirmProps(unsuspenedConfirmProps)
+  }
+
+  const setUnsuspendedSuccessProps = () => {
+    const unsuspendedSuccessProps = {
+      onClose: () => {
+        setIsSuccessModalOpen(false)
+      },
+      title: "ระบบโอนเงินไปยังหมอดูสำเร็จ",
+      info: "หมอดูได้รับเงินค่าดูดวงตามปกติ"
+    }
+    setSuccessProps(unsuspendedSuccessProps)
+  }
+
+  const setRefundConfirmProps = (report: ReportInfoType) => {
+    const refundConfirmProps: ConfirmOverlayProps = {
+      onClose: () => {
+        setIsConfirmModalOpen(false)
+      },
+      onConfirm: () => {
+        setIsConfirmModalOpen(false)
+        if (menuList[1] === "รายงานหมอดูไม่มาตามนัดหมาย") {
+          setRefundSuccsessProps(report)
+          setIsSuccessModalOpen(true)
+        }
+      },
+      target: report.reporterName,
+      type: "REJECT",
+      title: "ต้องการคืนเงินให้ลูกค้า",
+      message: "ใช่หรือไม่",
+      info: `หากกดยืนยันการคืนเงิน เงินจะถูกโอนกลับไปยังลูกค้า`
+    }
+    setConfirmProps(refundConfirmProps)
+  }
+
+  const setRefundSuccsessProps = (report: ReportInfoType) => {
+    const refundSuccessProps: SuccessModalProps = {
+      onClose: () => {
+        setIsSuccessModalOpen(false)
+        setbanConfirmProps(report)
+        setIsConfirmModalOpen(true)
+      },
+      title: "ระบบคืนเงินไปยังผู้ใช้สำเร็จ",
+      info: "ผู้ใช้ได้รับเงินคืนเรียบร้อย"
+    }
+    setSuccessProps(refundSuccessProps)
+  }
+
+  const setbanConfirmProps = (report: ReportInfoType) => {
+    const banConfirmProps: ConfirmOverlayProps = {
+      onClose: () => {
+        setIsConfirmModalOpen(false)
+      },
+      onConfirm: () => {
+        setIsConfirmModalOpen(false)
+        setSuccessProps(banSuccessProps)
+        setIsSuccessModalOpen(true)
+      },
+      target: report.reporteeName,
+      type: "REJECT",
+      title: "ต้องการแบนผู้ใช้",
+      message: "ใช่หรือไม่",
+      warning: `หากกดแบนไปแล้วผู้ใช้จะไม่สามารถใช้งาน MonDooDuang ได้อีก`
+    }
+    setConfirmProps(banConfirmProps)
+  }
+
+  const setFinishedConfirmProps = (report: ReportInfoType) => {
+    const finishConfirmProps: ConfirmOverlayProps = {
+      onClose: () => {
+        setIsConfirmModalOpen(false)
+      },
+      onConfirm: () => {
+        setIsConfirmModalOpen(false)
+      },
+      target: "ReportID : " + report.reportId,
+      type: "APPROVE",
+      title: "จัดการคำร้อง",
+      message: "เรียบร้อยแล้วใช่หรือไม่",
+      info: `หากกดยืนยันแล้วจะไม่สามารถจัดการหรือตรวจสอบคำร้องนี้ได้อีก`
+    }
+    setConfirmProps(finishConfirmProps)
+  }
+
+  const [isSuccessModalOpen, setIsSuccessModalOpen] = useState(false)
+  const [isErrorModalOpen, setIsErrorsModalOpen] = useState(false)
+  const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false)
+  const [confirmProps, setConfirmProps] = useState<ConfirmOverlayProps>(defaultConfirmProps)
+  const [successProps, setSuccessProps] = useState<SuccessModalProps>(defaultSuccessProps)
+  //
 
   useEffect(() => {
     const fetchReports = async () => {
@@ -76,6 +224,16 @@ export default function AdminReportManagementPage() {
     <>
       <NavBarAdmin menuFocus={"reportManagement"} username={username} />
       <div className="px-20 py-8">
+        <ConfirmOverlay isVisible={isConfirmModalOpen} confirmProps={confirmProps} />
+        <ErrorModal
+          isVisible={isErrorModalOpen}
+          onClose={() => {
+            setIsErrorsModalOpen(false)
+          }}
+          title={"ส่งคำขอจองไปที่หมอดูสำเร็จ"}
+          info={"ผู้ใช้งานนี้จะไม่สามารถใช้งาน\nMonDooDuang ได้อีกต่อไป"}
+        />
+        <SuccessModal isVisible={isSuccessModalOpen} successProps={successProps} />
         <div className="w-auto h-auto">
           <div className="flex flex-col justify-items-center items-center space-y-8">
             <div className="mt-10 text-white text-4xl font-noto-sans">จัดการคำร้องผู้ใช้</div>
@@ -103,7 +261,25 @@ export default function AdminReportManagementPage() {
                   }`}
                 >
                   {focusReports.map((report: ReportInfoType) => (
-                    <ReportCard report={report} />
+                    <ReportCard
+                      report={report}
+                      onClickFirstAction={() => {
+                        if (menuList[1] === "รายงานหมอดูไม่มาตามนัดหมาย") {
+                          setRefundConfirmProps(report)
+                        } else {
+                          setFinishedConfirmProps(report)
+                        }
+                        setIsConfirmModalOpen(true)
+                      }}
+                      onClickSecondAction={() => {
+                        if (menuList[1] === "รายงานหมอดูไม่มาตามนัดหมาย") {
+                          setUnsuspendedConfirmProps(report)
+                        } else {
+                          setbanConfirmProps(report)
+                        }
+                        setIsConfirmModalOpen(true)
+                      }}
+                    />
                   ))}
                 </div>
               ) : null}
--- frontend/src/pages/AdminReportManagementPage/components/ReportCard.tsx
+++ frontend/src/pages/AdminReportManagementPage/components/ReportCard.tsx
@@ -1,6 +1,57 @@
 import { ReportInfoType } from "../types/ReportInfoType"
 
-export default function ReportCard({ report }: { report: ReportInfoType }) {
+export default function ReportCard({
+  report,
+  onClickFirstAction,
+  onClickSecondAction
+}: {
+  report: ReportInfoType
+  onClickFirstAction?: () => void
+  onClickSecondAction?: () => void
+}) {
+  const InappropiateButton = () => {
+    return (
+      <div className="flex flex-col items-end justify-items-center space-y-3">
+        <button
+          onClick={onClickFirstAction}
+          style={{ transition: "background-color 0.3s" }}
+          className="w-[130px] py-1 px-2 justify-items-center items-center rounded-[10px] text-gray-200 text-md bg-mdd-sand-yellow hover:bg-mdd-sand-yellow-hover"
+        >
+          <div className="text-center">จัดการเรียบร้อย</div>
+        </button>
+
+        <button
+          onClick={onClickSecondAction}
+          style={{ transition: "background-color 0.3s" }}
+          className="w-[130px]  py-1 px-2 justify-items-center items-center rounded-[10px] text-gray-200 text-md bg-mdd-cancel-red hover:bg-red-600"
+        >
+          <div className="text-center">แบนผู้ใช้</div>
+        </button>
+      </div>
+    )
+  }
+
+  const MoneySuspendButton = () => {
+    return (
+      <div className="flex flex-col items-end justify-items-center space-y-3">
+        <button
+          onClick={onClickFirstAction}
+          style={{ transition: "background-color 0.3s" }}
+          className="w-[130px] py-1 px-2 justify-items-center items-center rounded-[10px] text-gray-200 text-md bg-mdd-sand-yellow hover:bg-mdd-sand-yellow-hover"
+        >
+          <div className="text-center">คืนเงินให้ลูกค้า</div>
+        </button>
+
+        <button
+          onClick={onClickSecondAction}
+          style={{ transition: "background-color 0.3s" }}
+          className="w-[130px]  py-1 px-2 justify-items-center items-center rounded-[10px] text-gray-200 text-md bg-mdd-muted-green hover:bg-mdd-muted-green-hover"
+        >
+          <div className="text-center">จ่ายเงินให้หมอดู</div>
+        </button>
+      </div>
+    )
+  }
   return (
     <div className="w-[650px] h-[200px] bg-white bg-opacity-50 rounded-2xl px-4 py-4 mb-8">
       <div
@@ -13,9 +64,9 @@ export default function ReportCard({ report }: { report: ReportInfoType }) {
         <div className="w-[100px] h-[100px] rounded-full bg-yellow-400"></div>
         <div className="flex flex-col w-[320px] justify-items-center text-md">
           <div className="text-mdd-focus-yellow">Report ID : {report.reportId}</div>
-          <div className="text-white">ผู้ถูกรายงาน : {report.reporterName}</div>
+          <div className="text-white">ผู้ถูกรายงาน : {report.reporteeName}</div>
           {report.reportType === "SYSTEM_ERROR" ? null : (
-            <div className="text-white">รายงานโดย : {report.reporteeName}</div>
+            <div className="text-white">รายงานโดย : {report.reporterName}</div>
           )}
           <div className="text-white">ปัญหาที่ถูกรายงาน : </div>
           <div className="max-h-[120px] text-mdd-cancel-red bg-white bg-opacity-80 rounded px-1 overflow-hidden shadow-inner">
@@ -28,50 +79,6 @@ export default function ReportCard({ report }: { report: ReportInfoType }) {
           <InappropiateButton />
         ) : null}
       </div>
-    </div>
-  )
-}
-
-const InappropiateButton = () => {
-  return (
-    <div className="flex flex-col items-end justify-items-center space-y-3">
-      <button
-        onClick={() => {}}
-        style={{ transition: "background-color 0.3s" }}
-        className="w-[130px] py-1 px-2 justify-items-center items-center rounded-[10px] text-gray-200 text-md bg-mdd-sand-yellow hover:bg-mdd-sand-yellow-hover"
-      >
-        <div className="text-center">จัดการเรียบร้อย</div>
-      </button>
-
-      <button
-        onClick={() => {}}
-        style={{ transition: "background-color 0.3s" }}
-        className="w-[130px]  py-1 px-2 justify-items-center items-center rounded-[10px] text-gray-200 text-md bg-mdd-cancel-red hover:bg-red-600"
-      >
-        <div className="text-center">แบนผู้ใช้</div>
-      </button>
-    </div>
-  )
-}
-
-const MoneySuspendButton = () => {
-  return (
-    <div className="flex flex-col items-end justify-items-center space-y-3">
-      <button
-        onClick={() => {}}
-        style={{ transition: "background-color 0.3s" }}
-        className="w-[130px] py-1 px-2 justify-items-center items-center rounded-[10px] text-gray-200 text-md bg-mdd-sand-yellow hover:bg-mdd-sand-yellow-hover"
-      >
-        <div className="text-center">คืนเงินให้ลูกค้า</div>
-      </button>
-
-      <button
-        onClick={() => {}}
-        style={{ transition: "background-color 0.3s" }}
-        className="w-[130px]  py-1 px-2 justify-items-center items-center rounded-[10px] text-gray-200 text-md bg-mdd-muted-green hover:bg-mdd-muted-green-hover"
-      >
-        <div className="text-center">จ่ายเงินให้หมอดู</div>
-      </button>
     </div>
   )
 }
