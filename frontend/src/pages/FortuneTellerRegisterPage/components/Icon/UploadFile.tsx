5171947695046f1b1b9aa06efbc32820702c667b
Namprik <92167161+Namprik@users.noreply.github.com>
2024-03-24 19:03:41 +0700

    [MD-3] Add real picture upload in fortune teller register (#72)

---
+++ frontend/src/pages/FortuneTellerRegisterPage/components/Icon/UploadFile.tsx
@@ -0,0 +1,5 @@
+import uploadFile from "../../../../assets/FortuneTellerRegisterAssets/uploadFile.png"
+
+export default function UploadFileIcon() {
+  return <img src={uploadFile} className="size-32" />
+}
