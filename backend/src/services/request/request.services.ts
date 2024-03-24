6720eb0d3d74c9f4a987368a513276d20516badb
its144prelationship <pastalonely@gmail.com>
2024-03-24 23:22:30 +0700

    fix: config

--- backend/src/services/request/request.services.ts
+++ backend/src/services/request/request.services.ts
@@ -1,7 +1,6 @@
 import { RequestStatus } from "../../models/request/request.model"
 import { requestRepository } from "../../repositories/request.repository"
 import { notificationRepository } from "../../repositories/notification.repository"
-import { s3Service } from "../infra/s3.services"
 
 export const requestService = {
   updateRequestStatus: async (requestId: string, status: RequestStatus) => {
@@ -15,19 +14,18 @@ export const requestService = {
     return { success: false, message: "error to change request status" }
   },
   getPendingRequest: async () => {
-    const requests = await requestRepository.getPendingRequest()
-    const updatedRequests = await Promise.all(requests.map(async (request) => {
-      const fortuneTellerId = request.fortuneTellerId?.toString() ?? ""
-      const profilePicData = await s3Service.downloadProfilePicture(fortuneTellerId as string)
-      if (profilePicData && profilePicData.ContentType !== undefined && profilePicData.ContentType !== null) {
-        request.profilePic = "data:image/jpg;base64," + profilePicData.Body?.toString("base64")
-      }
-      const idCardData = await s3Service.downloadIdCard(fortuneTellerId as string)
-      if (idCardData && idCardData.ContentType !== undefined && idCardData.ContentType !== null) {
-        request.approvalPic = "data:image/jpg;base64," + idCardData.Body?.toString("base64")
-      }
-      return request
-    }))
-    return updatedRequests
+    return await requestRepository.getPendingRequest()
+  },
+  updateToFortuneTellerTypeAndVerified: async (requestId: string) => {
+    const updateType = await requestRepository.updateFortuneTellerType(requestId)
+    if (!updateType.isSuccess) {
+      return { success: false, message: "error to change user type" }
+    }
+
+    const updateVerified = await requestRepository.updateFortuneTellerVerified(requestId)
+    if (!updateVerified.isSuccess) {
+      return { success: false, message: "error to change fortune teller verify" }
+    }
+    return { success: true, message: "user type and fortune teller verify have been changed" }
   }
 }
