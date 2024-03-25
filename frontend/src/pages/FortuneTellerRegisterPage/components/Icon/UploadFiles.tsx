568aa2da2d52fd99ad0e4c692b20ee6c37cf0cf9
Namprik <wareesa1847@gmail.com>
2024-03-25 14:37:54 +0700

    rename file

---
+++ frontend/src/pages/FortuneTellerRegisterPage/components/Icon/UploadFiles.tsx
@@ -0,0 +1,5 @@
+import uploadFiles from "../../../../assets/FortuneTellerRegisterAssets/uploadFiles.png"
+
+export default function UploadFileIcon() {
+  return <img src={uploadFiles} className="size-32" />
+}
