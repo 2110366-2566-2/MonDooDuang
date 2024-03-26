b6472b565d489613e7783de2e9df8557fff5dbf4
Namprik <wareesa1847@gmail.com>
2024-03-26 14:09:51 +0700

    lint

--- backend/src/services/review/review.services.ts
+++ backend/src/services/review/review.services.ts
@@ -5,13 +5,13 @@ import { CreateReviewSchema } from "../../models/review/review.model"
 export const reviewService = {
   createReview: async (review: CreateReviewSchema) => {
     const response = await reviewRepository.createReview(review)
-    if(!response) return { success: false, message: "error to create review" }
+    if (!response) return { success: false, message: "error to create review" }
 
     const updateReviewScore = await fortuneTellerRepository.updateReviewScore(review.fortuneTellerId, review.score)
     if (!updateReviewScore) {
       return { success: false, message: "error to update score" }
     }
 
-    return {success: true, message: "success to create review and score"}
+    return { success: true, message: "success to create review and score" }
   }
 }
